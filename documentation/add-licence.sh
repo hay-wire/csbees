2139506580e83d143cd7ada59beaebc21880f3aa
haywire <prashant.dwivedi@outlook.com>
2013-05-03 08:21:27 +0530

    added licence to all files

--- documentation/add-licence.sh
+++ documentation/add-licence.sh
@@ -2,6 +2,6 @@ for i in *.php
 do
   if ! grep -q Licence-Terms $i
   then
-    cat copyright.txt $i >$i.new && mv $i.new $i
+    cat ~/www/csbees/licence.txt $i > $i.new && mv $i.new $i
   fi
 done
--- views/wiki.php
+++ views/wiki.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+	Licence-Terms
+	===============
+	
+	This project is protected by Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported licence
+	Please read the online version of this licence here:
+		http://creativecommons.org/licenses/by-nc-sa/3.0/
+	
+	An offline copy is in the root folder of this project by the name "licence.pdf"
+
+	This licence is to keep the commercial fishes off the waters. Don't be disheartened. I am always an email away :)
+		Mail me at: prashant.dwivedi@outlook.com
+*/
+
+?>
+
+<?php
+
 
 function wiki()
 {	
